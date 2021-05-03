e19a2097513f13bf33818515c2cab7b41f7f6a34
akelle17 <35782969+akelle17@users.noreply.github.com>
2021-05-03 12:20:51 -0400

    init

---
+++ EmployeesApi/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    [Route("employees")]
+    public class EmployeesController : ControllerBase
+    {
+        // GET /
+        [HttpGet]
+        public ActionResult GetAllEmployees()
+        {
+            return Ok();
+        }
+
+        // GET/{id}
+
+        // POST /
+    }
+}
---
+++ EmployeesApi/Data/EmployeeDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Data
+{
+    public class EmployeeDataContext : DbContext
+    {
+        public EmployeeDataContext(DbContextOptions<EmployeeDataContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Employee> Employees { get; set; }
+    }
+}
